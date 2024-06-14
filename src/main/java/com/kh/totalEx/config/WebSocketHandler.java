ae3d32c63b40b7df4522d65923f914aa2e8b3e34
Lim JeongHu <dlaeocjf94@naver.com>
2024-06-14 09:42:01 +0900

    웹소켓 핸들러 추가 service, dto 구현 연습 + 스웨거 공부

--- src/main/java/com/kh/totalEx/config/WebSocketHandler.java
+++ src/main/java/com/kh/totalEx/config/WebSocketHandler.java
@@ -1,6 +1,54 @@
 package com.kh.totalEx.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kh.totalEx.dto.ChatMessageDto;
+import com.kh.totalEx.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequiredArgsConstructor
+@Slf4j
+@Component
 public class WebSocketHandler extends TextWebSocketHandler {
+    // 매핑을 해주는 것 ObjectMapper는 jackson -> json으로 파싱해주는 역할
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+    private final Map<WebSocketSession, String> sessionRoomIdMap = new ConcurrentHashMap<>(); // 세션 방아이디랑 매핑해주는것
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload(); // 클라이언트가 전송한 메시지
+        log.warn("{}", payload);
+        // JSON 문자열을 ChatMessageDto 객체로 변환
+        ChatMessageDto chatMessage = objectMapper.readValue(payload, ChatMessageDto.class);
+        String roomId = chatMessage.getRoomId(); // 채팅방 ID
+        // 세션과 채팅방 ID를 매핑
+        sessionRoomIdMap.put(session, roomId); // 세션과 채팅방 ID를 매핑
+        if (chatMessage.getType() == ChatMessageDto.MessageType.ENTER) { // 메시지 타입이 ENTER이면
+            chatService.addSessionAndHandleEnter(roomId, session, chatMessage); // 채팅방에 입장한 세션 추가
+        } else if (chatMessage.getType() == ChatMessageDto.MessageType.CLOSE) {
+            chatService.removeSessionAndHandleExit(roomId, session, chatMessage);
+        } else {
+            chatService.sendMessageToAll(roomId, chatMessage);
+        }
+
+    }
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        // 세션과 매핑된 채팅방 ID 가져오기
+        log.warn("afterConnectionClosed : {}", session);
+        String roomId = sessionRoomIdMap.remove(session);
+        if (roomId != null) {
+            ChatMessageDto chatMessage = new ChatMessageDto();
+            chatMessage.setType(ChatMessageDto.MessageType.CLOSE);
+            chatService.removeSessionAndHandleExit(roomId, session, chatMessage);
+        }
+    }
 }
